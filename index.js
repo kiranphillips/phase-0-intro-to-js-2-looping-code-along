68bd8f0e6f0c5e527bb738f22728c2b526b3af5a
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-02 09:27:55 -0600

    commit tree 3d92da2ee7f3395cada36855d1e117ec15956481

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+//const namesArray = ["Guadalupe,", "Ollie,", "Aki,"];
+function writeCards(arrayOfnames, event) {
+  let thankYoucard = []
+  for (let i = 0; i < arrayOfnames.length; i++) {
+    thankYoucard.push(`Thank you, ${arrayOfnames[i]}, for the wonderful ${event} gift!`);
+  }
+return thankYoucard
+}
+function countDown(countDown){
+while(countDown >= 0)
+console.log(countDown--);
+}
+//writeCards(namesArray);
